7e039170172d8ee6a37e0dd7ab3a474758f32f2d
Mwatu420 <wendee514@gmail.com>
2025-10-14 13:47:02 +0300

    first commit

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+const contactForm = document.getElementById("contactForm");
+const responseMessage = document.getElementById("responseMessage");
+
+contactForm.addEventListener("submit", function(event) {
+  event.preventDefault(); 
+
+  
+  responseMessage.style.display = "block";
+
+  
+  contactForm.reset();
+
+  setTimeout(() => {
+    responseMessage.style.display = "none";
+  }, 3000);
+});
+
